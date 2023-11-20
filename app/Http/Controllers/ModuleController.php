c4d7e4c937f740ff1d39178d3ee070f0e7a7f626
JordanNdjock <clothairegastalia@gmail.com>
2023-11-20 10:29:15 +0100

    1st itération

---
+++ app/Http/Controllers/ModuleController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ModuleController extends Controller
+{
+    public function index()
+    {
+        $currentYear = date('Y');
+        $startYear = 1990;
+        $years = range($currentYear, $startYear);
+    
+        return view('welcome', compact('years'));
+    }
+    public function TraitementForm(Request $request)
+    {
+        $allTitles = ['Enseignement', 'Encadrement', 'Comité de Lecture', 'Publication', 'Poste occupé', 'Projet de Recherche'];
+
+        $selectedTitles = $request->input('titles', []);
+
+        if (empty($selectedTitles)) {
+            $selectedTitles = $allTitles;
+        }
+
+        return view('report', ['selectedTitles' => $selectedTitles]);
+    }
+
+}
